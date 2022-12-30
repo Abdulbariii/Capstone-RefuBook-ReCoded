49e50d704fe92832f1e221674593ee281f200266
ShahlaKamal96 <shahla.kamal19@gmail.com>
2022-12-29 18:03:17 -0800

    completed about section inside home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import About from '../components/home-components/About';
 import Title from '../components/home-components/Title';
 
 export default function Home() {
   return <div>
     <Title />
+    <div className='bg-[#e5faff] '>
+      <About />
+    </div>
+
   </div>;
 }
