80565a4c0a9ac982c49843a25e74aa68c2f9cf6e
Hani Amer <cvba2222@gmail.com>
2023-01-02 22:29:47 +0300

    inspire completed and made responsive

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import Inspire from '../components/navigation/Inspire';
 
 export default function About() {
-  return <div className="text-7xl">About</div>;
+  return (
+  <div>
+    <h1>Navbar</h1>
+    <h1>Background Image</h1>
+    <h1>Our Story</h1>
+    <h1>Our Team</h1>
+    <h1>Testimonial</h1>
+    <h1>Partners</h1>
+    <Inspire/>
+    <h1>Footer</h1>
+  </div>
+  );
 }
