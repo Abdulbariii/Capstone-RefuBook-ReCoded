8882d183ee28422868e004d67de73792d883a6d9
Hani Amer <cvba2222@gmail.com>
2023-01-02 20:12:54 +0300

    Our story component created and responsive

--- src/components/navigation/Story.js
+++ src/components/navigation/Story.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 export default function Story() {
   return (
-    <div>
-        <h1>Our Story</h1>
+    <div className='flex flex-col flex-nowrap justify-center align-center bg-white'>
+        <div className=''>
+          <h1 className='mt-8 text-5xl md:mt-2 mb-8 text-blue-700 font-bold'>Our Story</h1>
+        </div>
+        <div className='mx-16 md:mx-72  mb-16'>
+          <p className='text-xl'>
+            It is a long established fact that a reader will 
+            be distracted by the readable content of a page when 
+            looking at its layout. The point of using Lorem Ipsum 
+            is that it has a more-or-less normal distribution 
+            of letters, as opposed to using Content here, content here, 
+            making it look like readable English.
+          </p>
+        </div>
     </div>
   )
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import Story from '../components/navigation/Story';
 
 export default function About() {
-  return <div className="text-7xl">About</div>;
+  return (
+  <div>
+    <h1>Navbar</h1>
+    <h1>Background Image</h1>
+    <div>
+      <Story/>
+    </div>
+    <h1>Our Team</h1>
+    <h1>Testimonial</h1>
+    <h1>Partners</h1>
+    <h1>Inspire</h1>
+    <h1>Footer</h1>
+  </div>
+  );
 }
