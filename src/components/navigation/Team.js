95e95480254db132e6bb4bb08c8bc6a0b5b6fd1f
Hani Amer <cvba2222@gmail.com>
2023-01-02 19:19:40 +0300

    testing react components

--- src/components/navigation/Team.js
+++ src/components/navigation/Team.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Team() {
-  return (
-    <div>
-        <h1>Our Team</h1>
-    </div>
-  )
+const Team = () => {
+    return (
+        <h2>Our Team</h2>
+    );
 }
+
+export default Team;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import Team from '../components/navigation/Team';
 
 export default function About() {
-  return <div className="text-7xl">About</div>;
+  return (
+  <div>
+    <h1>Navbar</h1>
+    <h1>Our Story</h1>
+    <div>
+      <Team/>
+    </div>
+    <h1>Testimonial</h1>
+    <h1>Partners</h1>
+    <h1>Inspire</h1>
+    <h1>Footer</h1>
+  </div>
+  );
 }
