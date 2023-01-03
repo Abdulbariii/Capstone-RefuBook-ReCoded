df3c4c161e7b4f064ce9d3c4ab8576c78c1ca4df
Hani Amer <cvba2222@gmail.com>
2023-01-03 21:27:03 +0300

    image to be added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import BackgroundImage from '../components/navigation/BackgroundImg';
 
 export default function About() {
-  return <div className="text-7xl">About</div>;
+  return (
+  <div>
+    <h1>NavBar</h1>
+    <div>
+      <BackgroundImage/>
+    </div>
+    <div>Our Story</div>
+    <h1>Our Team</h1>
+    <h1>Testimonial</h1>
+    <h1>Partners</h1>
+    <h1>Inspire</h1>
+    <h1>Footer</h1>
+  </div>
+  );
 }
