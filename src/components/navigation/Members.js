ef096d406e765cf3dd19bd86900558ca62041549
Hani Amer <cvba2222@gmail.com>
2023-01-04 20:17:30 +0300

    fixing lining of members component

--- src/components/navigation/Members.js
+++ src/components/navigation/Members.js
@@ -4,10 +4,10 @@ export default function Members() {
   return (
     <div className='flex flex-col flex-now text-[#4699C2] p-8 lg:p-14'>
         <div>
-            <h3 className='font-bold'>Emily Jorgensen</h3>
+            <h3 className='font-bold text-left'>Emily Jorgensen</h3>
         </div>
         <div>
-            <h3>Author at Panoply Store</h3>
+            <h3 className=''>Author at Panoply Store</h3>
         </div>
     </div>
   )
