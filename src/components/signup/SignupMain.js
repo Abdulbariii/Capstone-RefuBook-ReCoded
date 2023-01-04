2f5a906322aa5a35025831bc74e98983d26197ce
Hani Amer <cvba2222@gmail.com>
2023-01-04 21:48:18 +0300

    component main created

---
+++ src/components/signup/SignupMain.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function SignupMain() {
+  return (
+    <div className='my-96 m-14 bg-white rounded-3xl shadow-2xl'>
+        <div>
+            <h3>SignUp with</h3>
+        </div>
+        <div>
+            google
+        </div>
+        <div>
+            <h3>or</h3>
+        </div>
+        <div>
+            FaceBook
+        </div>
+    </div>
+  )
+}
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import SignupMain from '../components/signup/SignupMain';
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+  <div>
+    <h1>navbar</h1>
+    <div>
+      <SignupMain/>
+    </div>
+    <h1>SignUp Footer</h1>
+  </div>
+  );
 }
