4e77c526cb2fb64c332be0ee82cef17d3fcc19ae
Hani Amer <cvba2222@gmail.com>
2023-01-07 23:28:07 +0300

    form for desktop created and useState updated

--- src/components/profile/Form.js
+++ src/components/profile/Form.js
@@ -1,33 +1,66 @@
-import React from 'react'
+import { useState } from 'react';
 
 export default function Form() {
-  return (
-    <form className='flex flex-col w-4/6'>
-        <h2 className='text-left text-[#4699C2] font-bold py-2'>Name: </h2>
-        <div className='border border-gray-300 rounded-md'>
-            <input type='text' className='opacity-0 w-full py-2'/>
-        </div>
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [biograhpy, setBiography] = useState('');
+    const [location, setLocation] = useState('');
 
-        <h2 className='text-left text-[#4699C2] font-bold py-2'>Surname: </h2>
-        <div className='border border-gray-300 rounded-md'>
-            <input type='text' className='opacity-0 w-full py-2'/>
+  return (
+    <form className='flex flex-col w-4/6 lg:w-3/6'>
+
+        <div className='lg:flex lg:flex-row lg:justify-between lg:gap-6'>
+            <div className='lg:flex lg:flex-col lg:w-1/2'>
+                <h2 className='text-left text-[#4699C2] font-bold py-2'>Name: </h2>
+                <div className='border border-gray-300 rounded-md'>
+                    <input 
+                    type='text' 
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className='w-full py-2 opacity-50 focus:opacity-100'/>
+                </div>
+            </div>
+
+            <div className='lg:flex lg:flex-col lg:w-1/2'>
+                <h2 className='text-left text-[#4699C2] font-bold py-2'>Surname: </h2>
+                <div className='border border-gray-300 rounded-md'>
+                    <input 
+                    type='text'
+                    required
+                    value={surname}
+                    onChange={(e) => setSurname(e.target.value)} 
+                    className='opacity-50 focus:opacity-100 w-full py-2'/>
+                </div>
+            </div>
         </div>
 
         <h2 className='text-left text-[#4699C2] font-bold py-2'>Biograhpy: </h2>
         <div className='border border-gray-300 rounded-md'>
-            <textarea required className='opacity-0 w-full py-4'>
+            <textarea 
+            required
+            value={biograhpy}
+            onChange={(e) => setBiography(e.target.value)} 
+            className='opacity-50 focus:opacity-100 w-full py-4'>
             testing
             </textarea>
         </div>
 
         <h2 className='text-left text-[#4699C2] font-bold py-2'>Location: </h2>
         <div className='border border-gray-300 rounded-md'>
-            <input className='opacity-0 w-full py-2'/>
+            <input 
+            required
+            value={location}
+            onChange={(e) => setLocation(e.target.value)} 
+            className='opacity-50 focus:opacity-100 w-full py-2'/>
         </div>
 
-        <div className='flex flex-row justify-around py-4'>
-            <input type='submit' value='SAVE' className='bg-[#4699C2] hover:bg-blue-700 text-white fong-bold w-24 py-3 rounded-full'/>
-            <input type='reset' value='CANCEL' className='bg-[#4699C2] hover:bg-blue-700 text-white fong-bold w-24 py-3 rounded-full'/>
+        <div className='flex flex-row justify-center py-4'>
+            <input type='submit' value='SAVE' className='bg-[#4699C2] text-white fong-bold w-24 py-3 rounded-full mx-4 font-bold hover:bg-[#026FC2] hover:shadow-lg focus:bg-[#026FC2] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#026FC2] active:shadow-lg transition duration-150 ease-in-out'/>
+            <input
+            type='reset' 
+            value='CANCEL' 
+            className='bg-[#4699C2] lg:bg-white hover:bg-[#026FC2] hover:text-white hover:shadow-lg focus:bg-[#026FC2] focus:shadow-lg focus:outline-none focus:ring-0 focus:text-white active:bg-[#026FC2] active:shadow-lg transition duration-150 ease-in-out text-white lg:text-[#4699C2] lg:border lg:border-[#4699C2] fong-bold w-24 py-3 rounded-full font-bold'/>
         </div>
     </form>
   )
