651928edd93607b06603139f4eebd2d67f889fa4
Hani Amer <cvba2222@gmail.com>
2023-01-09 18:01:38 +0300

    made the array of objects for the members and mapped over them

--- src/components/navigation/Members.js
+++ src/components/navigation/Members.js
@@ -1,14 +1,60 @@
 import React from 'react'
 
 export default function Members() {
+  const members = [
+    {
+      name: 'Emily Jorgensen',
+      location: 'Author at Panoply Store',
+      image: ''
+    },
+    {
+      name: 'Emily Jorgensen',
+      location: 'Author at Panoply Store',
+      image: ''
+    },
+    {
+      name: 'Emily Jorgensen',
+      location: 'Author at Panoply Store',
+      image: ''
+    },
+    {
+      name: 'Emily Jorgensen',
+      location: 'Author at Panoply Store',
+      image: ''
+    },
+    {
+      name: 'Emily Jorgensen',
+      location: 'Author at Panoply Store',
+      image: ''
+    },
+    {
+      name: 'Emily Jorgensen',
+      location: 'Author at Panoply Store',
+      image: ''
+    },
+    {
+      name: 'Emily Jorgensen',
+      location: 'Author at Panoply Store',
+      image: ''
+    },
+    {
+      name: 'Emily Jorgensen',
+      location: 'Author at Panoply Store',
+      image: ''
+    }
+  ]
   return (
-    <div className='flex flex-col flex-now text-[#4699C2] p-8 lg:p-14'>
-        <div>
-            <h3 className='font-bold text-left'>Emily Jorgensen</h3>
+    <div className='flex flex-row flex-wrap justify-center px-16 lg:mb-10'>
+      {members.map((member) => (
+        <div className='flex flex-col flex-nowrap text-[#4699C2] p-8 lg:p-14'>
+          <div>
+            <h3 className='font-bold text-left'>{member.name}</h3>
+          </div>
+          <div>
+            <h3 className=''>{member.location}</h3>
+          </div>
         </div>
-        <div>
-            <h3 className=''>Author at Panoply Store</h3>
-        </div>
+      ))}
     </div>
   )
 }
