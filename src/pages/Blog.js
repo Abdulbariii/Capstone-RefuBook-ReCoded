08d014a08e803167fd3fd452446594d521843142
Wafaa.Alhayek <wafash.alhayek@gmail.com>
2023-01-10 21:06:38 +0200

    card design for the blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,5 +1,71 @@
 import React from 'react';
 
-export default function Blogs() {
-  return <div className="text-7xl">Blogs</div>;
-}
+export default function Blog() {
+    return (
+        <div>
+        <div className=" flex lg:flex-row lg:gap-16 flex-col items-center gap-8 justify-center mt-28">
+
+<div className="card1 w-64 group bg-white">
+      <img src="https://images.unsplash.com/photo-1593642532744-d377ab507dc8" alt="blog pic" className="rounded-t-lg mt-12" />
+  <div className="bg-white rounded-lg shadow-2xl max-h-96">
+    <div className="p-6">
+      <h2 className="font-bold text-l mb-2 text-black">Title</h2>
+      <p className="text-black hidden group-hover:inline-block">
+        Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dolor sitdada Lorem ipsum dolor sitdad
+      </p>
+      <div className="flex items-center">
+          <img className="w-10 h-10 rounded-full mr-4" src="src/assests/images/girl.jpg" alt="Avatar"/>
+          <div className="text-sm">
+            <p className="text-sky-500 leading-none">Bruce Lee</p>
+          </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+<div className="card2 w-64 group bg-white">
+      <img src="https://images.unsplash.com/photo-1593642532744-d377ab507dc8" alt="blog pic" className="rounded-t-lg" />
+  <div className="bg-white rounded-lg shadow-2xl max-h-96 -mb-10">
+    <div className="p-6">
+      <h2 className="font-bold text-l text-black mb-2">Title</h2>
+      <p className="text-black hidden group-hover:inline-block">
+        Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dolor sitdada Lorem ipsum dolor sitdad
+      </p>
+      <div className="flex items-center">
+          <img className="w-10 h-10 rounded-full mr-4" src="src/assests/images/girl.jpg" alt="Avatar"/>
+          <div className="text-sm">
+            <p className="text-sky-500 leading-none">Bruce Lee</p>
+          </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+<div className="card3 w-64 group bg-white">
+      <img src="https://images.unsplash.com/photo-1593642532744-d377ab507dc8" alt="blog pic" className="rounded-t-lg" />
+  <div className="bg-white rounded-lg shadow-2xl max-h-96 -mb-10">
+    <div className="p-6">
+      <h2 className="font-bold text-l text-black mb-2">Title</h2>
+      <p className="text-black hidden group-hover:inline-block">
+        Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dolor sitdada Lorem ipsum dolor sitdad
+      </p>
+      <div className="flex items-center">
+          <img className="w-10 h-10 rounded-full mr-4" src="src/assests/images/girl.jpg" alt="Avatar"/>
+          <div className="text-sm">
+            <p className="text-sky-500 leading-none">Bruce Lee</p>
+          </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+</div>
+
+
+  
+      </div>
+    );
+  }
