0558935052c860bba8806270e720f3670c3f2ee2
Wafaa.Alhayek <wafash.alhayek@gmail.com>
2023-01-16 21:18:42 +0200

    single blog page

---
+++ src/pages/SingleBlogPage.js
@@ -0,0 +1,77 @@
+import React from 'react'
+
+export default function SingleBlogPage() {
+  return (
+    <div>
+
+
+    <div classNameName="flex lg:flex-row flex-col">
+<div className="m-7">
+  <h1 className="font-Roboto mb-2 text-center font-bold">Title Title Title Title Title Title</h1>
+  <img src="https://images.unsplash.com/photo-1545529468-42764ef8c85f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80" alt="blog photo" className="m-auto w-96 rounded-md drop-shadow-lg shadow-gray-800 " />
+        <div className=" flex justify-items-end p-2 w-52 m-auto">
+          <p className="text-sm m-3"> by</p>
+          <img className="w-10 h-10 rounded-full m-auto" src="https://assets.materialup.com/uploads/b78ca002-cd6c-4f84-befb-c09dd9261025/preview.png" alt="Avatar"/> 
+          <div className="text-sm">
+            <p className="text-sky-500 leading-none m-3">Bruce Lee</p>
+          </div>
+        </div>
+     <div className="flex-wrap lg:flex-none">
+  <h3 className="m-auto w-72 text-justify font-semibold text-sm p-2  ">subTitle subTitle subTitle subTitle subTitle subTitle subTitle subTitle subTitle subTitle</h3>
+  </div>
+   <p className="text-justify whitespace-pre-wrap lg:mx-56 leading-normal"> <br>Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dLorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dolor </br>
+<br> Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dLorem ipsum dolor sitadad Lorem ipsum Lorem ipsum dolor sitadad Lorem ipsum orem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dolor </br>
+Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum Lorem ipsum dolor sitadad Lorem ipsum dolor sitadad Lorem ipsum dolor </p>
+
+
+</div>
+
+<div className=" border-t-gray-400 border-t-2 lg:hidden"></div>
+
+
+<div className="flex lg:flex-col lg:justify-center">
+  
+  <div className="justify-start ml-2 w-24">
+  <p className=" font-semibold lg:-ml-20"> Read also: </p>
+  </div>
+<div className="w-52 group bg-white content-end mt-9 -ml-20 mb-11 lg:mr-4">
+      <img src="https://images.unsplash.com/photo-1593642532744-d377ab507dc8" alt="blog pic" className="rounded-t-lg " />
+  <div className="bg-white rounded-lg shadow-2xl max-h-62">
+    <div className="p-6">
+      <h2 className="font-bold text-l mb-2 text-black">Title</h2>
+      
+      <div className="flex items-center">
+          <img className="w-10 h-10 rounded-full mr-4" src="src/assests/images/girl.jpg" alt="Avatar"/>
+          <div className="text-sm">
+            <p className="text-sky-500 leading-none">Bruce Lee</p>
+          </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+<div className="w-52 group bg-white content-end mt-9 lg:-ml-20 ml-5 mb-11">
+      <img src="https://images.unsplash.com/photo-1593642532744-d377ab507dc8" alt="blog pic" className="rounded-t-lg " />
+  <div className="bg-white rounded-lg shadow-2xl max-h-62">
+    <div className="p-6">
+      <h2 className="font-bold text-l mb-2 text-black">Title</h2>
+      
+      <div className="flex items-center">
+          <img className="w-10 h-10 rounded-full mr-4" src="src/assests/images/girl.jpg" alt="Avatar"/>
+          <div className="text-sm">
+            <p className="text-sky-500 leading-none">Bruce Lee</p>
+          </div>
+    </div>
+  </div>
+</div>
+</div>
+
+</div>
+</div>    
+   
+
+
+    </div>
+  )
+}
