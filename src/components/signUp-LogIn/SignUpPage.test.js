a9237029a53a5779d442dbbc45894fa5932f9084
Abdulbari <abdulbari.112917@gmail.com>
2023-01-21 22:38:10 +0300

    fixing issue

--- src/components/signUp-LogIn/SignUpPage.test.js
+++ src/components/signUp-LogIn/SignUpPage.test.js
@@ -1,26 +1,23 @@
+/* eslint-disable import/order */
 import React from 'react';
 import { render } from '@testing-library/react';
 import SignUpPage from './SignUpPage';
-import { BrowserRouter } from "react-router-dom"
-
+import { BrowserRouter } from 'react-router-dom';
 
 const MockSignUpPage = () => {
-    return (
-        <BrowserRouter>
-            <SignUpPage />
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <SignUpPage />
+    </BrowserRouter>
+  );
+};
 
 describe('SignUpPage', () => {
-    test('matches snapshot', () => {
-        const { asFragment } = render(<MockSignUpPage />);
-        expect(asFragment()).toMatchSnapshot();
-    });
-    test('rendering sign up page', () => {
-        render(<MockSignUpPage />);
-    });
+  test('matches snapshot', () => {
+    const { asFragment } = render(<MockSignUpPage />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  test('rendering sign up page', () => {
+    render(<MockSignUpPage />);
+  });
 });
-
-
-
